b4e7e0f1a820818796bfb6792967da1591d98346
XelorR <xelor@ya.ru>
2021-01-29 01:22:38 +0300

    all the rest parsed too

---
+++ process_001to193_195to210.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+def parse_file(issue_number: str = "193"):
+
+    f = open(f"{issue_number}.html", encoding="utf-8")
+    soup = BeautifulSoup(f, "lxml")
+    issue_content = soup.find("div", attrs={"id": "issue-content"})
+    content = [e.text.split(". ", 1) for e in issue_content.find_all("li")]
+
+    with open(f"{issue_number}.csv", "w", encoding="utf-8") as c:
+        import csv
+        csvwriter = csv.writer(c)
+        csvwriter.writerow("issue name content".split())
+        for i in content:
+            csvwriter.writerow([f"{issue_number}"] + i)
+
+    f.close()
+
+for i in list(range(1, 194)) + list(range(195, 211)):
+    print("Processing file", i)
+    parse_file(str(i).zfill(3))
+
