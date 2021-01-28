32f5651ef988f2d628d596e6f835e5d521727b30
XelorR <xelor@ya.ru>
2021-01-29 00:26:16 +0300

    redirecting option added

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # wider available version:
-# for i in {001..259}; do echo downloading $i; curl -so $i.html  https://xakep.ru/issues/xa/$i; done
+# for i in {001..259}; do echo downloading $i; curl -sLo $i.html  https://xakep.ru/issues/xa/$i; done
 
 parallel wget -O {}.html https://xakep.ru/issues/xa/{.} ::: {001..259}
 js-beautify -l 2 -r *.html
