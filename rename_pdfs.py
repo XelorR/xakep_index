ed2f6945b911a590a81c42e5b83068c236805944
XelorR <xelor@ya.ru>
2021-03-27 08:15:37 +0300

    file renamer function added

---
+++ rename_pdfs.py
@@ -0,0 +1,17 @@
+import re
+import os
+
+files = [f for f in os.listdir("pdf") if f.endswith(".pdf")]
+
+
+def clear_name(f: str) -> str:
+    if "(" in f:
+        return (re.findall(r"\((\d+)\)", f)[0]+".pdf").zfill(7)
+    else:
+        try:
+            return re.findall(r"\d+", f)[0]+".pdf"
+        except IndexError:
+            return f
+
+
+{f:clear_name(f) for f in files}
